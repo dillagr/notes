b4ff726940d0a3d894ad236382aea68fe35b4339
Great Dilla <35280192+dillagr@users.noreply.github.com>
2020-07-07 11:22:55 +0800

    Create sample-pandas-code.py

---
+++ python-code/sample-pandas-code.py
@@ -0,0 +1,21 @@
+## requires: import libraries
+# import pandas as pd
+# import os
+
+sample_df = pd.DataFrame()
+
+for col in df.columns.to_list():
+	
+	## create this directory, if it doesn't exist
+    sample_bin = "_explore-directory"
+    if not os.path.exists(sample_bin):
+        os.makedirs(sample_bin)
+    
+    ## output file, based on column header
+    sample_obj = os.path.join(sample_bin, f"explore-{col}.csv")
+    sample_df = pd.DataFrame(df[col].value_counts())
+    sample_df.columns = ["count"]
+    sample_df.index.name = col
+    sample_df.to_csv(sample_obj, index=True, sep="|", quoting=csv.QUOTE_NONE)
+    ## QUOTE_MINIMAL, QUOTE_ALL, QUOTE_NONE, and QUOTE_NONNUMERIC
+    
