270f8c8bb4acdd85bab513d73accf5137b72bdda
Great Dilla <35280192+dillagr@users.noreply.github.com>
2020-07-09 15:09:43 +0800

    Update sample-pandas-anti-join.py

--- python-code/sample-pandas-anti-join.py
+++ python-code/sample-pandas-anti-join.py
@@ -4,6 +4,10 @@
 ## dfA = pd.DataFrame() <-- define dfA
 ## dfB = pd.DataFrame() <-- define dfB
 
+## NOTE!
+## when "indicator=True" is used, "_merge" column will contain either
+##   of these three values: "left_only", "right_only" or "both"
+
 ## right anti-join
 df = pd.merge(dfA, dfB, how="outer", indicator=True)
 df[df['_merge'] == "left_only"]
